425da723c1ea8527985a639868e784ccbdefdbe2
aliibakhtiari <77485073+aliibakhtiari@users.noreply.github.com>
2021-04-09 07:23:01 +0430

    new project

---
+++ index.js
@@ -0,0 +1,25 @@
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  localStorage.setItem("to do list",inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+}
+
+function get_item(){
+  var GetData= localStorage.getItem("to do list");
+  inputValue.innerHTML = GetData;
+}
